ab13b9ddba2c05995a5faf09837de50b0c2a69f1
viladit <70838553+viladit@users.noreply.github.com>
2025-03-10 00:44:33 +0300

    5th task done

---
+++ src/main/java/BasicPart1/singleOccurrenceNumber_129.java
@@ -0,0 +1,22 @@
+package BasicPart1;
+
+public class singleOccurrenceNumber_129 {
+    public static void main(String[] args) {
+        System.out.println("""
+            ============================================
+            Program to find a number that appears only once in a given array of integers. All numbers occur twice.
+            ============================================
+            Example: {10,2,38,23,38,23,21}
+            Output: 23
+            ============================================
+        """);
+        System.out.println("Example: {10,20,30,40,30,20,10}");
+        int[] array = {10,20,30,40,30,20,10};
+        int result = 0;
+        for (int num : array) {
+            result ^= num;
+        }
+        System.out.println("Output: " + result);
+
+    }
+}
