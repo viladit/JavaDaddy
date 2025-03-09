15471b0be12dde879669ace0d788e1bded78d7cf
viladit <viladit@gmail.com>
2025-03-09 23:27:47 +0300

    First task done

---
+++ src/main/java/BasicPart1/SumOfTwoNumbers_2.java
@@ -0,0 +1,30 @@
+package BasicPart1;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbers_2 {
+    public static void main(String[] args) {
+        System.out.println("""
+            ============================================
+            Program for summing to numbers
+            ============================================
+            Type 2 numbers to get their sum.
+            Example:
+            Enter first number: 5
+            Enter second number: 10
+            Result: 15
+            ============================================
+        """);
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.print("Enter first number: ");
+            int FirstNum = input.nextInt();
+            System.out.print("Enter second number: ");
+            int SecondNum = input.nextInt();
+            int Result = FirstNum + SecondNum;
+            System.out.println("Result: " + Result);
+        } catch (Exception e) {
+            System.out.println("Wrong input!");
+        }
+    }
+}
