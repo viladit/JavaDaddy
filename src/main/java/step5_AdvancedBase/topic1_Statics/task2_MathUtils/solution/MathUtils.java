79beb5a0cefd7a42ad26bad97d91735defd8e343
viladit <viladit@gmail.com>
2025-03-19 22:03:02 +0300

    5.1.2 Done

---
+++ src/main/java/step5_AdvancedBase/topic1_Statics/task2_MathUtils/solution/MathUtils.java
@@ -0,0 +1,9 @@
+package step5_AdvancedBase.topic1_Statics.task2_MathUtils.solution;
+
+public class MathUtils {
+    private MathUtils() {}
+
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+}
---
+++ src/main/java/step5_AdvancedBase/topic1_Statics/task2_MathUtils/solution/MathUtilsDemo.java
@@ -0,0 +1,8 @@
+package step5_AdvancedBase.topic1_Statics.task2_MathUtils.solution;
+
+public class MathUtilsDemo {
+    public static void main(String[] args) {
+        System.out.println(MathUtils.max(1,123123));
+        System.out.println(MathUtils.max(500, 250));
+    }
+}
