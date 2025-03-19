a5492c200d768dbe928741543b1f6ce192807927
viladit <viladit@gmail.com>
2025-03-20 00:47:01 +0300

    5.2.1 Done

---
+++ src/main/java/step5_AdvancedBase/topic2_Generics/task1_Pair/solution/PairDemo.java
@@ -0,0 +1,8 @@
+package step5_AdvancedBase.topic2_Generics.task1_Pair.solution;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        Pair pair = new Pair("Hello", 1000);
+        System.out.println(pair);
+    }
+}
