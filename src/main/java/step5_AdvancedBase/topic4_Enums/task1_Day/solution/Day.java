7f22b2e63ab1b5598a10b59ec2ad387c017b1560
viladit <viladit@gmail.com>
2025-03-21 00:39:09 +0300

    5.4.1 Done

---
+++ src/main/java/step5_AdvancedBase/topic4_Enums/task1_Day/solution/Day.java
@@ -0,0 +1,11 @@
+package step5_AdvancedBase.topic4_Enums.task1_Day.solution;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/step5_AdvancedBase/topic4_Enums/task1_Day/solution/DayDemo.java
@@ -0,0 +1,10 @@
+package step5_AdvancedBase.topic4_Enums.task1_Day.solution;
+
+
+public class DayDemo {
+    public static void main(String[] args) {
+        for (Day day : Day.values()) {
+            System.out.println(day);
+        }
+    }
+}
